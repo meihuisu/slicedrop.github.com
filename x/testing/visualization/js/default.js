6be64fea7f8f59c6005e22f92dabc8fab3ec4bf9
Mei-Hui Su <mei@jacoby.isi.edu>
2014-12-09 15:58:36 -0800

    1) add a copy of recent x into ui

---
+++ x/testing/visualization/js/default.js
@@ -0,0 +1,12 @@
+  console.log('Testing the DEV tree.');
+  
+  // includes
+  goog.require('X.renderer2D');
+  goog.require('X.renderer3D');
+  goog.require('X.fibers');
+  goog.require('X.mesh');
+  goog.require('X.volume');
+  goog.require('X.cube');
+  goog.require('X.sphere');
+  goog.require('X.cylinder');
+  
