d56cca597e821963549bf961df070e4dc285c173
Mei-Hui Su <mei@DragonsGate.local>
2015-06-10 21:38:45 -0700

    1) make tests work on chrome and safari   a) select needs to be trigger on 'change' instead of 'click' for      chrome   b) flickering mesh problem due to rendering/opacity

--- js/x.controller.js
+++ js/x.controller.js
@@ -333,8 +333,11 @@ function volumerenderingOnOff(bool) {
   }
 
   if (bool) {
-// no need to initialize it
-//    volume.lowerThreshold = (volume.min + (volume.max/10));
+// to initialize it only if it is the first time
+    if( (volume.min == volume.lowerThreshold ) &&
+            (volume.max == volume.upperThreshold ) ) {
+      volume.lowerThreshold = (volume.min + (volume.max/10));
+    }
   }
 
   volume.volumeRendering = bool;
@@ -688,6 +691,7 @@ function toggleMeshSelector() {
 
 // meshColor
   var _hex=RGBTohex(mesh.color);
+window.console.log("MEI, XXX reset meshColor square "+_hex);
   jQuery('#meshColor').miniColors("value", _hex);
 }
 
